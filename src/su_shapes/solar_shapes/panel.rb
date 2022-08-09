9144ae032e9d6d25498c2d72851e1848c282f7b4
jackmase <jsmason04@gmail.com>
2022-08-09 11:24:41 -0500

    add new file for panel object

---
+++ src/su_shapes/solar_shapes/panel.rb
@@ -0,0 +1,15 @@
+
+require_relative '../shapes/box'
+
+module CommunityExtensions::Shapes
+
+  # Generic base class for solar panel
+  class Panel < Box
+
+    def initialize
+
+    end
+
+  end
+
+end
